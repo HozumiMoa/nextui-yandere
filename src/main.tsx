740e9e3577952e4bc4346ee1996004fd564b9423
UiShigure <574668563@qq.com>
2024-03-02 16:10:17 +0800

    Add react-router-dom package and update dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,29 @@ import { NextUIProvider } from '@nextui-org/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { DeviceProvider } from './context/device.tsx'
 import { ThemeProvider } from './context/theme.tsx'
 import './index.css'
+
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+    },
+  ],
+  {
+    basename: '/nextui-yandere',
+  }
+)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <NextUIProvider>
       <ThemeProvider>
         <DeviceProvider>
-          <App />
+          <RouterProvider router={router} />
         </DeviceProvider>
       </ThemeProvider>
     </NextUIProvider>
