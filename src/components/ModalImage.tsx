77d7206ccda9a09522df0e2d04222006956b16c4
UiShigure <574668563@qq.com>
2024-03-03 18:28:48 +0800

    Refactor ModalImage component

--- src/components/ModalImage.tsx
+++ src/components/ModalImage.tsx
@@ -22,35 +22,33 @@ export default function ModalImage(props: Props): React.ReactElement {
   }
 
   return (
-    <>
-      <Modal
-        backdrop="blur"
-        hideCloseButton
-        isOpen={isOpen}
-        onOpenChange={onOpenChange}
-        onKeyUp={handleKeyUp}
-        classNames={{
-          base: 'm-auto',
-          wrapper: 'items-center',
-        }}
-        style={{
-          // 模态框宽度不超过屏幕宽度的 90%，高度不超过屏幕高度的 90%，保持图片比例
-          maxWidth: `min(90dvw, calc(90dvh * ${sample_width} / ${sample_height}), ${sample_width}px)`,
-          height: `min(90dvh, calc(90dvw * ${sample_height} / ${sample_width}), ${sample_height}px)`,
-        }}
-      >
-        <ModalContent>
-          <Image src={sample_url} width={sample_width} height={sample_height} />
-          <div
-            className="absolute left-0 z-10 h-full w-1/3 cursor-[url(@/assets/arrow_back_ios.svg),_pointer]"
-            onClick={() => onModalPageChange('prev')}
-          ></div>
-          <div
-            className="absolute right-0 z-10 h-full w-1/3 cursor-[url(@/assets/arrow_forward_ios.svg),_pointer]"
-            onClick={() => onModalPageChange('next')}
-          ></div>
-        </ModalContent>
-      </Modal>
-    </>
+    <Modal
+      backdrop="blur"
+      hideCloseButton
+      isOpen={isOpen}
+      onOpenChange={onOpenChange}
+      onKeyUp={handleKeyUp}
+      classNames={{
+        base: 'm-[unset] sm:m-[unset]',
+        wrapper: 'items-center',
+      }}
+      style={{
+        // 模态框宽度不超过屏幕宽度的 90%，高度不超过屏幕高度的 90%，保持图片比例
+        maxWidth: `min(90dvw, calc(90dvh * ${sample_width} / ${sample_height}), ${sample_width}px)`,
+        height: `min(90dvh, calc(90dvw * ${sample_height} / ${sample_width}), ${sample_height}px)`,
+      }}
+    >
+      <ModalContent>
+        <Image src={sample_url} width={sample_width} height={sample_height} />
+        <div
+          className="absolute left-0 z-10 h-full w-1/3 cursor-[url(@/assets/arrow_back_ios.svg),_pointer]"
+          onClick={() => onModalPageChange('prev')}
+        ></div>
+        <div
+          className="absolute right-0 z-10 h-full w-1/3 cursor-[url(@/assets/arrow_forward_ios.svg),_pointer]"
+          onClick={() => onModalPageChange('next')}
+        ></div>
+      </ModalContent>
+    </Modal>
   )
 }
